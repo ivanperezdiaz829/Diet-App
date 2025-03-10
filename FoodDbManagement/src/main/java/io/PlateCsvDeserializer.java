cdf429c7df5b480c7462e68517511a7964a031b1
DaniSosa <dnsosdom@gmail.com>
2025-03-10 19:24:41 +0000

    Added a javascript code to load dishes from csv files and created the food database

---
+++ FoodDbManagement/src/main/java/io/PlateCsvDeserializer.java
@@ -0,0 +1,33 @@
+package io;
+
+import model.Plate;
+
+public class PlateCsvDeserializer implements PlateDeserializer {
+
+    @Override
+    public Plate deserialize(String line) {
+        return deserialize(line.split(","));
+    }
+
+    private Plate deserialize(String[] fields) {
+        return new Plate(
+                fields[0],
+                toInt(fields[1]),
+                toInt(fields[2]),
+                toInt(fields[3]),
+                toInt(fields[4]),
+                toInt(fields[5]),
+                toInt(fields[6]),
+                toFloat(fields[7]),
+                toInt(fields[8])
+        );
+    }
+
+    private float toFloat(String field) {
+        return !(field.equals("\\N")) ? Float.parseFloat(field) : (float) -1.;
+    }
+
+    private int toInt(String field) {
+        return !(field.equals("\\N")) ? Integer.parseInt(field) : -1;
+    }
+}
