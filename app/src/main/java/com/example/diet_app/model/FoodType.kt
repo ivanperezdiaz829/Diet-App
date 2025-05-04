f05dca9939fb8610489a2e98f2b80e38d2edb031
Daniel <dmoralessosa@gmail.com>
2025-05-04 12:32:30 +0100

    Cargar la dieta generada

--- app/src/main/java/com/example/diet_app/model/FoodType.kt
+++ app/src/main/java/com/example/diet_app/model/FoodType.kt
@@ -2,8 +2,21 @@ package com.example.diet_app.model
 
 enum class FoodType {
     LIGHT_MEAL,
-    SIDE_DISH,
     MAIN_DISH,
+    SIDE_DISH,
     DRINK,
-    DESSERT
+    DESSERT;
+
+    companion object {
+        fun fromTypeId(typeId: Int): FoodType {
+            return when (typeId) {
+                1  ->  LIGHT_MEAL
+                2  ->  MAIN_DISH
+                3  ->  SIDE_DISH
+                4  ->  DRINK
+                5  ->  DESSERT
+                else -> throw IllegalArgumentException("Unknown food type ID: $typeId")
+            }
+        }
+    }
 }
